bb35207ebdeaa7bacfc0c76710111c58994c3857
deepak325268 <deepaktest14@gmail.com>
2026-07-11 09:59:52 +0530

    Increment MANUAL_VERSION to 22

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 
 // 1) MANUAL VERSION — jab bhi khud se force-update chahiye,
 //    bas ye number badha dena (1 -> 2 -> 3...)
-var MANUAL_VERSION = 21;
+var MANUAL_VERSION = 22;
 
 // 2) AUTO VERSION — mahina badalte hi khud-ba-khud badal jata hai
 var now = new Date();
